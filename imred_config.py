7360e746846d29ee9269e737aa0b37031852b770
Nick Moskovitz <nmosko@ommegang.local>
2021-06-03 13:07:32 +0800

    add support for pre-upgrade SOAR/Goodman

--- imred_config.py
+++ imred_config.py
@@ -9,7 +9,8 @@ import sys
 
 # telescope/instrument configurations
 implemented_instruments = ['hall_nasa42','DCT_lmi','SOAR 4.1m_Goodman Spectro',
-                           '2.1m Otto Struve_','31-in_NASAcam','GTC_OSIRIS']
+                           '2.1m Otto Struve_','31-in_NASAcam','GTC_OSIRIS',
+                           'SOAR 4.1m_Goodman Spectrograph']
 
 
 # GTC, OSIRIS
@@ -154,7 +155,7 @@ dct_lmi = {
     'trim' : True
 }
 
-# SOAR, Goodman
+# SOAR, Goodman, post 2016
 soar_goodman = {
     
     # instrument-specific FITS header keywords
@@ -182,6 +183,34 @@ soar_goodman = {
     'trim' : False
 }
 
+# SOAR, Goodman, pre 2016
+soar_goodman_old = {
+    
+    # instrument-specific FITS header keywords
+    'time': 'UT',  # UT time at start of exposure
+    'airmass': 'AIRMASS',   # airmass of observation
+    'binning': ('CCDSUM#blank0', 'CCDSUM#blank1'),    # x and y bin factors
+    'target_name': 'OBJECT',  # target name keyword
+    'filter': 'FILTER',  # filter keyword
+    'exptime': 'EXPTIME',  # exposure time keyword (s)
+    'data_region': 'TRIMSEC',   # trim region keyword
+    
+    'frame_type': 'OBSTYPE',   # keyword for type of image
+    'bias': 'BIAS',  # bias frame_type
+    'skyflat': 'FLAT',  # twilight flat frame_type
+    'domeflat': 'FLAT',  # dome flat frame_type
+    'object': 'OBJECT',  # object frame_type
+    
+    # instrument specific properties
+    'bias_counts': 460.,    # typical bias frame counts
+    'max_counts': 52000.,   # max counts before non-linear
+    
+    # instrument specific processing steps
+    'bias_correct' : True,
+    'flat_correct' : True,
+    'trim' : False
+}
+
 
 # translate telescope+instrument keywords into parameter set defined here
 params = {
@@ -189,6 +218,7 @@ params = {
     'DCT_lmi':                      dct_lmi,
     '2.1m Otto Struve_':            mcdonald2m_cquean,
     'SOAR 4.1m_Goodman Spectro':    soar_goodman,
+    'SOAR 4.1m_Goodman Spectrograph':    soar_goodman_old,
     '31-in_NASAcam':                lowell31in_nasacam,
     'GTC_OSIRIS':                   gtc_osiris
 }
