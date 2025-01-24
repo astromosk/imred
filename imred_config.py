19d3a06989796bf2cb8d8cbd9d1a874da86af3aa
Nick Moskovitz <nmosko@lowell.edu>
2025-01-24 13:41:28 -0700

    add support for LDT/POETS

--- imred_config.py
+++ imred_config.py
@@ -10,8 +10,36 @@ import sys
 # telescope/instrument configurations
 implemented_instruments = ['hall_nasa42','DCT_lmi','SOAR 4.1m_Goodman Spectro',
                            '2.1m Otto Struve_','31-in_NASAcam','GTC_OSIRIS',
-                           'SOAR 4.1m_Goodman Spectrograph','NPOI_PW1000-1_FLI']
+                           'SOAR 4.1m_Goodman Spectrograph','NPOI_PW1000-1_FLI','2.2m UH_SNIFS','LDT_POETS']
 
+
+# LDT, POETS
+ldt_poets = {
+    
+    # instrument-specific FITS header keywords
+    'time': 'UT',  # UT time at start of exposure
+    'airmass': 'AIRMASS',   # airmass of observation
+    'binning': ('XBINNING', 'YBINNING'),    # x and y bin factors
+    'target_name': 'OBJECT',  # target name keyword
+    'filter': 'FILTER',  # filter keyword
+    'exptime': 'EXPTIME',  # exposure time keyword (s)
+    'data_region': 'TRIMSEC',   # trim region keyword
+    
+    'frame_type': 'IMAGETYP',   # keyword for type of image
+    'bias': 'BIAS',  # bias frame_type
+    'skyflat': 'FLAT',  # twilight flat frame_type
+    'domeflat': 'FLAT',  # dome flat frame_type
+    'object': 'OBJECT',  # object frame_type
+    
+    # instrument specific properties
+    'bias_counts': 500.,    # typical bias frame counts
+    'max_counts': 60000.,   # max counts before non-linear
+    
+    # instrument specific processing steps
+    'bias_correct' : True,
+    'flat_correct' : True,
+    'trim' : False
+}
 
 # GTC, OSIRIS
 gtc_osiris = {
@@ -239,6 +267,34 @@ soar_goodman_old = {
     'trim' : False
 }
 
+# UH2.2m, SNIFS
+uh88_snifs = {
+    
+    # instrument-specific FITS header keywords
+    'time': 'UTC',  # UT time at start of exposure
+    'airmass': 'AIRMASS',   # airmass of observation
+    'binning': ('CCDSUM#blank0', 'CCDSUM#blank1'),    # x and y bin factors
+    'target_name': 'OBJECT',  # target name keyword
+    'filter': 'FILTER',  # filter keyword
+    'exptime': 'EXPTIME',  # exposure time keyword (s)
+    'data_region': 'DATASEC',   # trim region keyword
+    
+    'frame_type': 'OBSTYPE',   # keyword for type of image
+    'bias': 'BIAS',  # bias frame_type
+    'skyflat': 'SKY',  # twilight flat frame_type
+    'domeflat': 'DOME',  # dome flat frame_type
+    'object': 'OBJECT',  # object frame_type
+    
+    # instrument specific properties
+    'bias_counts': 460.,    # typical bias frame counts
+    'max_counts': 50000.,   # max counts before non-linear
+    
+    # instrument specific processing steps
+    'bias_correct' : False,
+    'flat_correct' : True,
+    'trim' : True
+}
+
 
 # translate telescope+instrument keywords into parameter set defined here
 params = {
@@ -249,7 +305,9 @@ params = {
     'SOAR 4.1m_Goodman Spectrograph':    soar_goodman_old,
     '31-in_NASAcam':                lowell31in_nasacam,
     'GTC_OSIRIS':                   gtc_osiris,
-    'NPOI_PW1000-1_FLI':            pw1m_fli
+    'NPOI_PW1000-1_FLI':            pw1m_fli,
+    '2.2m UH_SNIFS':                uh88_snifs,
+    'LDT_POETS':                    ldt_poets
 }
 
 def parameters(tel_inst):
