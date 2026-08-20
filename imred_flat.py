81f8f7b7e72d253c7bc053fb1b7e060a40d5ab6f
Nick Moskovitz <nmosko@lowell.edu>
2026-08-20 08:30:46 -0700

    update import of ModeEstimatorBackground from photutils

--- imred_flat.py
+++ imred_flat.py
@@ -13,7 +13,7 @@ import numpy as np
 from astropy.io import fits
 from astropy.table import Table
 from scipy import stats
-from photutils import ModeEstimatorBackground
+from photutils.background import ModeEstimatorBackground
 
 # initialize background class
 bkg=ModeEstimatorBackground()
